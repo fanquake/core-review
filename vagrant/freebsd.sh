f104fac5122d863e4aaf7e3cfb1a47109f780f75
fanquake <fanquake@gmail.com>
2019-12-06 10:46:43 -0500

    freeBSD: use RELEASE containers Add PR setup Only build bdb on provision Don't install OpenSSL

--- vagrant/freebsd.sh
+++ vagrant/freebsd.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 provision() {
-    pkg install --yes autoconf \
+    pkg install --yes \
+        autoconf \
         automake \
         boost-libs \
         git \
@@ -9,40 +10,35 @@ provision() {
         libevent \
         libtool \
         libzmq4 \
-        openssl \
         pkgconf \
-        python37
+        python37 # 3.7 for 11 & 12
 
     git clone https://github.com/bitcoin/bitcoin
+
+    ./bitcoin/contrib/install_db4.sh `pwd`
 }
 
-compile() {
+setup() {
 
     cd bitcoin
 
-    git stash && git checkout master && git clean -fxd
+    git clean -fxd && git stash && git checkout master
 
-    # Install BerkeleyDB
-    ./contrib/install_db4.sh `pwd`
-    export BDB_PREFIX=/usr/home/vagrant/bitcoin/db4
-
-    ./autogen.sh
-    # --disable-dependency-tracking to work around automake issues
-    # https://github.com/bitcoin/bitcoin/issues/14404
-    ./configure --with-gui=no --disable-dependency-tracking \
-        BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" \
-        BDB_CFLAGS="-I${BDB_PREFIX}/include"
-    gmake -j4
+    if [ -z "$2" ]
+    then
+        git fetch origin pull/$1/head:$1
+        git checkout $1
+    fi
 }
 
 case $1 in
 	provision)
 		provision
 	;;
-	compile)
-        compile
+	setup)
+        setup $2
 	;;
 	*)
-	    echo "Usage: freebsd.sh provision|compile"
+	    echo "Usage: freebsd.sh provision|setup PR"
 	;;
 esac
