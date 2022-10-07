4cfb4702464ceab3399dd68ef10edc53de8f67df
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-07 22:35:04 +0000

    Adding tarfile member sanitization to extractall()

--- apple-os.py
+++ apple-os.py
@@ -50,7 +50,26 @@ def build_git(library: str, versions: List[str]):
 		download(library, f)
 
 		with tarfile.open(f, 'r') as tar:
-			tar.extractall('repos/', numeric_owner=True)
+def is_within_directory(directory, target):
+	
+	abs_directory = os.path.abspath(directory)
+	abs_target = os.path.abspath(target)
+
+	prefix = os.path.commonprefix([abs_directory, abs_target])
+	
+	return prefix == abs_directory
+
+def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+
+	for member in tar.getmembers():
+		member_path = os.path.join(path, member.name)
+		if not is_within_directory(path, member_path):
+			raise Exception("Attempted Path Traversal in Tar File")
+
+	tar.extractall(path, members, numeric_owner=numeric_owner) 
+	
+
+safe_extract(tar, "repos/", numeric_owner="True")
 		os.remove(f)
 		untar = 'repos/{}-{}/'.format(library, version)
 
